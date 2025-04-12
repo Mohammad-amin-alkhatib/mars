6203fadd2eca27ed64fbbcc9cac0fd48517cc4a8
diabalsaadiOP <diab.alsaadi@optimumpartners.co>
2025-04-13 00:12:16 +0300

    try for dynamic route

---
+++ src/pages/media/[id]/index.js
@@ -0,0 +1,37 @@
+// Packages
+import React from "react";
+import path from "path";
+import fs from "fs";
+// Components
+import InrtoHeader from '@/components/IntroHeader';
+
+const NewsPage = ({ header }) => {
+    return (
+        <>
+            <InrtoHeader header={header} />
+        </>
+    )
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const { id } = params;
+    const filePath = path.join(process.cwd(), `src/data/news/${id}.json`);
+    const jsonData = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(jsonData) || {};
+
+
+    return {
+        props: {
+            ...data,
+        },
+    };
+}
+
+export default NewsPage
