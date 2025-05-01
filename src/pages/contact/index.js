d9cb27c1fa8163b48b68b1c3860b543fe969eb39
Hanna Mdanat <yuhanamdanat@gmail.com>
2025-05-01 22:26:13 +0300

    WIP

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -33,17 +33,25 @@ const ContactPage = ({ header, getIntouch }) => {
             />
             <div className={styles.contactUs}>
                 <Form className={styles.form} />
-                <div className={styles.location}>
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17691.23926105935!2d35.88355437238305!3d32.53950057680667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151c76f0573efd0b%3A0xa49ded1f5dbe5871!2sMARSRobotics!5e0!3m2!1sar!2sjo!4v1744405256436!5m2!1sar!2sjo"
-                        width="100%"
-                        height="100%"
-                        frameborder="0"
-                        allowfullscreen="true"
-                        loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"
-                    ></iframe>
+                <div className={styles.infoContainer}>
+                    <div className={styles.location}>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17691.23926105935!2d35.88355437238305!3d32.53950057680667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151c76f0573efd0b%3A0xa49ded1f5dbe5871!2sMARSRobotics!5e0!3m2!1sar!2sjo!4v1744405256436!5m2!1sar!2sjo"
+                            width="100%"
+                            height="100%"
+                            frameborder="0"
+                            allowfullscreen="true"
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
+                        ></iframe>
 
+                    </div>
+                    <div className={styles.info}>
+                        <p>Email: info@marsrobotic.com</p>
+                        <p>Phone: +962 27102026</p>
+                        <p>Fax: +962 27102044</p>
+                        <p>Address: Wasfi Al Tal Street, P.O Box 2233, Irbid 21163, Jordan</p>
+                    </div>
                 </div>
             </div>
         </div>
