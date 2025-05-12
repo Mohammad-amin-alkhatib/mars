d1ca2a6170f4b0834869946cdb215e5b0aa9e4d6
diabalsaadiOP <diab.alsaadi@optimumpartners.co>
2025-05-12 22:12:04 +0300

    wip

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -6,12 +6,12 @@ const Description = ({ items }) => {
     <div className={styles.descContainer}>
       {items.map((item) => (
         <div key={item.title} className={styles.descWrapper}>
-            {!!item?.imgSrc && <img src={`/${item.imgSrc}`} />}
-            {!!item?.videoUrl && <video src={`/${item.videoUrl}`} controls className={styles.video}/>}
-            <div className={styles.titleContainer}>
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-            </div>
+          {!!item?.imgSrc && <img src={`/${item.imgSrc}`} />}
+          {!!item?.videoUrl && <video src={`/${item.videoUrl}`} controls className={styles.video} />}
+          <div className={styles.titleContainer}>
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+          </div>
         </div>
       ))}
     </div>
