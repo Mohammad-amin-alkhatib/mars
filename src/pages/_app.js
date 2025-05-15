7be84f4b8ac3724c26497d57f46526beb02efc99
diabalsaadiOP <diab.alsaadi@optimumpartners.co>
2025-05-16 01:40:32 +0300

    wip

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,17 +12,20 @@ export default function App({ Component, pageProps }) {
         <Head>
           {title && <title>{title}</title>}
           {description && <meta name="description" content={description} />}
+          <meta content="Mars" property="og:site_name" />
           {description && <meta name="og:description" content={description} />}
-          {description && <meta name="twitter:description" content={description} />}
           {title && <meta name="og:title" content={title} />}
-          {title && <meta name="twitter:title" content={title} />}
+          {imageUrl && <meta property="og:image" content={imageUrl} />}
           {keywords && <meta name="keywords" content={keywords} />}
+          <meta content="summary" name="twitter:card" />
+          <meta content="@mars" name="twitter:site" />
+          {imageUrl && <meta content={imageUrl} name="twitter:image" />}
+          {title && <meta content={title} name="twitter:title" />}
+          {description && <meta content={description} name="twitter:description" />}
           <meta content="width=device-width,initial-scale=1,maximum-scale=5" name="viewport" />
           <meta charSet="utf-8" />
           <meta content="#000000" name="theme-color" />
           {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
-          {imageUrl && <meta property="og:image" content={imageUrl} />}
-          {imageUrl && <meta name="twitter:image" content={imageUrl} />}
           {imageUrl && <meta content={imageUrl} itemProp="thumbnailURL" />}
           <link href='/mars-logo.svg' rel="shortcut icon" />
           <meta content="origin-when-cross-origin" name="referrer" />
